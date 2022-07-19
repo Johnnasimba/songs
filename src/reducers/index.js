3a5d413321484897a7225f28bbe9e4e449c191bf
John <nasimba4john@gmail.com>
2022-07-19 10:08:33 +0200

    reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,33 @@
+const songsReducer = () => {
+  return [
+    {
+      title: 'Lithe',
+      duration: '4.20'
+    },
+    {
+      title: 'Ndilinawo mwayi',
+      duration: '3.75'
+    },
+    {
+      title: 'bachalo soja',
+      duration: '5.5'
+    },
+    {
+      title: 'Kukongola kumapha',
+      duration: '6.25'
+    },
+    {
+      title: 'Mwikho',
+      duration: '4.09'
+    }
+  ]
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+  switch (action.type) {
+    case 'SELECT_SONG':
+      return action.payload
+    default:
+      return selectedSong
+  }
+}
